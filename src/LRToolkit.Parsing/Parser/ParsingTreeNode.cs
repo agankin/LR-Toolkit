84724f98d3cebba944795f5372d8c1d655cf64fd
Andrei Gankin <gankin89@gmail.com>
2023-09-28 23:20:47 +0400

    Missed file.

--- src/LRToolkit.Parsing/Parser/ParsingTreeNode.cs
+++ src/LRToolkit.Parsing/Parser/ParsingTreeNode.cs
@@ -1,22 +1,30 @@
-﻿namespace LRToolkit.Parsing;
+﻿using LRToolkit.Lexing;
+using Optional;
+using Optional.Unsafe;
+
+namespace LRToolkit.Parsing;
 
 public readonly record struct ParsingTreeNode<TSymbol> where TSymbol : notnull
 {
-    public required Symbol<TSymbol> Symbol { get; init; }
+    public required readonly TSymbol Symbol { get; init; }
+
+    public required readonly Option<Lexem<TSymbol>> Lexem { get; init; }
 
     public required IEnumerable<ParsingTreeNode<TSymbol>> Nodes { get; init; }
 
     public static ParsingTreeNode<TSymbol> ForShift(Symbol<TSymbol> symbol) =>
         new ParsingTreeNode<TSymbol>
         {
-            Symbol = symbol,
+            Symbol = symbol.Value.ValueOrFailure(),
+            Lexem = symbol.Lexem,
             Nodes = Enumerable.Empty<ParsingTreeNode<TSymbol>>()
         };
 
     public static ParsingTreeNode<TSymbol> ForReduce(Symbol<TSymbol> symbol, IEnumerable<ParsingTreeNode<TSymbol>> nodes) =>
         new ParsingTreeNode<TSymbol>
         {
-            Symbol = symbol,
+            Symbol = symbol.Value.ValueOrFailure(),
+            Lexem = symbol.Lexem,
             Nodes = nodes
         };
 }
